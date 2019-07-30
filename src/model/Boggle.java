b955c7d7632735055a3c093982f7d5ca319552d5
jrace <jrace@DESKTOP-8U2ORN7>
2019-07-29 19:04:53 -0700

    Passes all junit tests in DiceTrayGradingTest.

--- src/model/Boggle.java
+++ src/model/Boggle.java
@@ -1,7 +1,9 @@
 package model;
 
 public class Boggle {
-
+	public Boggle() {
+		
+	}
   // TODO: Complete a Boggle game that will be used with two views
   //  1. A console based game with standard IO  (Boggle Two)
   //  2. An event-driven program with a graphical user interface (Boggle Three)
---
+++ src/model/DiceTray.java
@@ -0,0 +1,142 @@
+package model;
+
+import java.util.HashSet;
+
+/**
+ * @author Jacob Phelps
+ * @file DiceTray.java
+ *
+ *       Date: Jul 29, 2019
+ *       Course CSC: 210
+ *       Assignment: 34 - Boggle 1
+ */
+public class DiceTray {
+
+	int ROWS;
+	int COLS;
+	HashSet<String> collection = new HashSet<String>();
+	private char[][] board;
+
+	/**
+	 * Construct a tray of dice using a hard coded 2D array of chars. Use this
+	 * for testing
+	 * 
+	 * @param newBoard
+	 *                 The 2D array of characters used in testing
+	 */
+	public DiceTray(char[][] newBoard) {
+		ROWS = newBoard.length;
+		COLS = newBoard[0].length;
+		board = newBoard;
+		makeArrayCharsUpperCase();
+
+	}
+
+	/**
+	 * Converts the board and makes all characters uppercase so that the game is
+	 * case insensitive.
+	 */
+	private void makeArrayCharsUpperCase() {
+		char[][] tmp = new char[ROWS][COLS];
+		for (int i = 0; i < ROWS; i++) {
+			for (int j = 0; j < COLS; j++) {
+				tmp[i][j] = Character.toUpperCase(board[i][j]);
+			}
+		}
+		board = tmp;
+	}
+
+	/**
+	 * Using recursive backtracking and pruning, this function only visits die
+	 * that are making progress towards the search term.
+	 * 
+	 * @param visited
+	 *                The 2D boolean array of which die have been visited on
+	 *                this branch
+	 * @param i
+	 *                The current row position on the board.
+	 * @param j
+	 *                The current col position on the board
+	 * @param str
+	 *                The currently built string for this branch
+	 */
+	private void wordFinder(boolean visited[][], int i, int j, String str) {
+		visited[i][j] = true;
+		str = str + board[i][j];
+
+		// Prune out invalid options
+		if (str.equals(findThis)) {
+			wordFound = true;
+			return;
+		} else if (breakBranch(str)) {
+			visited[i][j] = false;
+			return;
+		}
+
+		// Check neighboring die
+		for (int row = i - 1; row <= i + 1 && row < ROWS; row++) {
+			for (int col = j - 1; col <= j + 1 && col < COLS; col++) {
+				if (row >= 0 && col >= 0) {
+					if (!visited[row][col]) {
+						wordFinder(visited.clone(), row, col, str);
+					}
+				}
+			}
+		}
+		// Undo changes
+		str = "" + str.charAt(str.length() - 1);
+		visited[i][j] = false;
+	}
+
+	/**
+	 * Description: This function determines if the string that is currently
+	 * being built is making progress towards search term. If it is not, it
+	 * returns false to prevent that are impossible.
+	 * 
+	 * @param str
+	 * @return boolean
+	 */
+	private boolean breakBranch(String str) {
+		int strLen = str.length();
+		int fndLen = findThis.length();
+		if (strLen == fndLen) {
+			return true;
+		} else {
+			for (int i = 0; i < str.length(); i++) {
+				if (str.charAt(i) != findThis.charAt(i)) {
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+
+	/**
+	 * Return true if search is word that can found on the board following the
+	 * rules of Boggle
+	 * 
+	 * @param str
+	 *            A word that may be in the board by connecting consecutive
+	 *            letters
+	 * @return True if search is found
+	 */
+
+	boolean wordFound;
+	private String findThis = "";
+
+	public boolean found(String attempt) {
+		wordFound = false;
+		findThis = attempt.toUpperCase();
+
+		boolean[][] visited = new boolean[ROWS][COLS];
+		for (int i = 0; i < ROWS; i++) {
+			for (int j = 0; j < COLS; j++) {
+				if (board[i][j] == findThis.charAt(0)) {
+					wordFinder(visited, i, j, "");
+				}
+			}
+		}
+		return wordFound;
+	}
+
+}
